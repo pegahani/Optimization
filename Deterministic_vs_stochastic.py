9518b988831b473f4f8aea5ae48d82db600e3623
root <root@douglas.info.unicaen.fr>
2018-04-02 15:47:59 +0200

    code without bug

--- Deterministic_vs_stochastic.py
+++ Deterministic_vs_stochastic.py
@@ -1,36 +1,37 @@
+from Cython.Compiler.FlowControl import GV
+
 from minmax_regret import reload_mdp, load_mdp, minmax_regret
-
-
 
 def main():
     text_file = open("./results/deterministic_vs_stochastic.txt", "w")
     _gamma = 0.9
-    for _state in range(4,10): #51
-        for _action in range(2,5): #11
-            for _seed in range(1,10):
+    for _state in range(3,4): #51
+        for _action in range(4,5): #11
+            for _seed in range(561,562):
                 print "**************************"
                 print "**************************"
-                print "_state _action, _gamma, _seed : " , _state, " , " , _action, " , " , _gamma, " , " , _seed 
+                print "_state _action, _gamma, _seed : " , _state, " , " , _action, " , " , _gamma, " , " , _seed
                 print "**************************"
                 print "**************************"
-                #load_mdp(_state, _action, _gamma, _seed)
-                _mdp = reload_mdp(_state, _action, _gamma, _seed)
+                #load_mdp(_state, _action, _gamma, _seed, _reward_lb= -10, _reward_up= 10 )
+                _mdp = reload_mdp(_state, _action, _gamma, _seed, _reward_lb= -10, _reward_up= 10)
                 minmax = minmax_regret(_mdp, [-1, 1])
-                text_file.write(str(_state)+ ';' + str(_action) + ';' + str(_seed) + ';')
-                minmax.solve_deterministic_opt(60,0.01)
-                text_file.write( str(minmax.UB) #optimal stochastic policy value
-                     + ';' + str(minmax.ROOT_LB)  #optimal deterministic policy value
-                     + ';' + str(minmax.BB_tot_nodes) #total number of BB nodes
-                     + ';' + str(minmax.BB_nodes_pruned) # total nodes pruned for BB
-                     + ';' + str(minmax.MASTER_tot_cuts) # total cuts on BB nodes
-                     + ';' + str(minmax.ROOT_tot_cuts) #total number of constraints <g,r> added to solve master program
-                     + ';' + str(minmax.TIME_master) # total computing time for master
-                     + ";" + str(minmax.TIME_slave) # total computing time for slave
-                     + ';' + str(minmax.TIME_root) # total computing time for benders decomposition
-                     + ';' + str(minmax.TIME_limit_reached) # flag equal to true if time limit is reached
-                     )
-                text_file.write(minmax.output)
-                text_file.write('\n')
+                minmax.solve_deterministic_opt_stack(60,0.01)
+                if (abs(minmax.UB-minmax.ROOT_LB)/minmax.UB) *100 >= 0.05 :
+                    text_file.write(str(_state) + ';' + str(_action) + ';' + str(_seed) + ';')
+                    text_file.write( str(minmax.UB) #optimal deterministic policy value
+                         + ';' + str(minmax.ROOT_LB)  #optimal stochastic policy value
+                         + ';' + str(minmax.BB_tot_nodes) #total number of BB nodes
+                         + ';' + str(minmax.BB_nodes_pruned) # total nodes pruned for BB
+                         + ';' + str(minmax.MASTER_tot_cuts) # total cuts on BB nodes
+                         + ';' + str(minmax.ROOT_tot_cuts) #total number of constraints <g,r> added to solve master program
+                         + ';' + str(minmax.TIME_master) # total computing time for master
+                         + ";" + str(minmax.TIME_slave) # total computing time for slave
+                         + ';' + str(minmax.TIME_root) # total computing time for benders decomposition
+                         + ';' + str(minmax.TIME_limit_reached) # flag equal to true if time limit is reached
+                         )
+                    text_file.write(minmax.output)
+                    text_file.write('\n')
 
     text_file.close()
     pass
